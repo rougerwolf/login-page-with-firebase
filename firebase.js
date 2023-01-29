73b87728048efb7aa2fbf397d91a1c1633f4d9b7
Deepak Singh Rana <91240645+rougerwolf@users.noreply.github.com>
2023-01-29 10:38:22 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+var app_firebase = {};
+(function () {
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyB31hRkffHg57uLr80Qtsz9bv1m8ZgeZFk",
+        authDomain: "try1-5cbd6.firebaseapp.com",
+        projectId: "try1-5cbd6",
+        storageBucket: "try1-5cbd6.appspot.com",
+        messagingSenderId: "1025401362032",
+        appId: "1:1025401362032:web:b8cd26511fb342897dc6b5"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    app_firebase = firebase;
+})()
